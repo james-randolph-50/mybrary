e0a974ca2eb1128fa83e793e9c5793653c3ddf45
James <james.randolph2016@gmail.com>
2018-10-15 19:19:11 -0400

    render info users

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import InfoDisplay from './components/InfoDisplay';
 
 class App extends Component {
   render() {
