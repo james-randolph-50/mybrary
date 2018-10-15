134fc7ffc4bc4fa3cc3764e5e04d67b51afa4339
James <james.randolph2016@gmail.com>
2018-10-15 19:47:30 -0400

    onchange and onsubmit functions

--- client/components/InfoEntry.js
+++ client/components/InfoEntry.js
@@ -4,6 +4,28 @@ class InfoEntry extends Component {
     constructor(props) {
         super(props)
 
-        
+        this.state = {
+            name: ''
+            books_read: ''
+        }
     }
+
+    handleOnChange = event => {
+        const { name, value } = event.target;
+        this.setState({
+            [name]: value
+        })
+    }
+
+    handleOnSubmit = event => {
+        event.preventDefault();
+        const user = this.state;
+        this.props.addUser(user)
+        this.setState({
+            name: '',
+            books_read: ''
+        })
+    }
+
+    
 }
