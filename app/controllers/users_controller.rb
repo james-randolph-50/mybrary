9264cd9498225a4ef2d4f8b8bd39068c65b72ead
James <james.randolph2016@gmail.com>
2018-10-15 20:12:37 -0400

    create users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,19 @@ class Api::UsersController < ApplicationController
         render json: @users
     end
 
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            render json: @user
+        else
+            render json: { errprs: {message: "User could not be saved."}}
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :books_read)
+    end
+
 end
