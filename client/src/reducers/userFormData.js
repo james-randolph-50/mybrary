f3e763b8b6d54671ccb1e07acd9381cee9c4470d
James <james.randolph2016@gmail.com>
2018-10-16 18:58:23 -0400

    finished userFormData

--- client/src/reducers/userFormData.js
+++ client/src/reducers/userFormData.js
@@ -7,6 +7,12 @@ export default (state = initialState, action) => {
 
     switch(action.type) {
         case 'UPDATED_DATA':
-            return action.userFormData
+            return action.userFormData;
+
+        case 'RESET_USER_FORM':
+            return initialState;
+        
+        default:
+            return state;
     }
 }
